8c20f5a7804f0e5a54ff7f801164e77870b8e222
Ajay Singh <117065425+ajaysingh266@users.noreply.github.com>
2023-04-15 10:26:31 +0530

    contact and filter added

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -4,11 +4,12 @@ return(
   <BrowserRouter>
   <Routes>
   <Route to="/" element={<Home/>}/>
-<Route to="/about" element={<About/>}/>
-  <Route to="/*" element={<Page404/>}/>
+  <Route to="/about" element={<About/>}/>
+ // <Route to="/*" element={<Page404/>}/>
      <Route to="/user/:name" element={<User/>}/>
     <Route to="/*" element={<Navigate to ="/"/>}/>
-  
+    <Route path = "/filter" element = {<Filter/>}/>
+  <Route path = "/contact" element = {<Contact/>} />
   </Routes>
   </BrowserRouter>
 </>
